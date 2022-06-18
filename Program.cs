9c225929669e36423c5209ac7389c74dae4431e2
Janny Nyendwa <nyendwa@yahoo.com>
2022-06-18 09:01:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Write a function that takes an array of integers as an argument
+//and returns a value based on the sums of the even and odd numbers in the array.
+//Let X = the sum of the odd numbers in the array and let Y = the sum of the even numbers.
+//The function should return X – Y
+using System;
+
+namespace SumOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumEven = 0;
+            int sumOdd = 0;
+            int[] a = new int[] { 1, 2, 4, 5, 9};
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    sumEven += a[i];
+                }
+                else
+                {
+                    sumOdd += a[i];
+                }
+                
+            }
+            Console.WriteLine(sumOdd - sumEven);
+        }
+    }
+}
